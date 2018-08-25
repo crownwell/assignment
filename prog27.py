008efc2b3960b2b2930d9e96721d5db0392d4e1e
joshua <joshuacronwell12@gmail.com>
2018-08-25 17:55:17 +0100

    Add files via upload

---
+++ prog27.py
@@ -0,0 +1,8 @@
+# A python program to convert a decimal number to binary,octal and hexadecimal
+
+dec=int(input("Enter the decimal number: "))
+
+print("the binary value of",dec,"is",bin(dec))
+print("the octal value of",dec,"is",oct(dec))
+print("the hexadecimal value of",dec,"is",hex(dec))
+
